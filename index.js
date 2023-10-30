0c7af0087ed93c121645d4d81a6b3966c72c0e37
elishevaisaacs18 <elishevaisaacs@hilma.tech>
2023-10-30 15:43:02 +0200

    set local storage

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-try {
-    function loadDoc(url, callback) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                callback(JSON.parse(this.responseText).results);
-            }
-        }
+function loadDoc(key) {
+    const xhttp = new XMLHttpRequest()
+    xhttp.responseType = "json";
+    xhttp.onreadystatechange = function () {
+        xhttp.onload = () => localStorage.setItem(key, JSON.stringify(xhttp.response.results));
     }
-    xhttp.open("GET", url);
+    xhttp.open("GET", "https://swapi.dev/api/" + key);
     xhttp.send();
 }
-catch (err) {
-    console.log("we had a problem reaching the server:( try again later!")
-}
 
-getPeople = () => {
-
-}
-
-// loadDoc("https://swapi.dev/api/people", getPeople);
-
-getFilms = () => {
-    
-}
-
-getShips = () => {
-    
-}
+loadDoc("people");
+loadDoc("films");
+loadDoc("species");
